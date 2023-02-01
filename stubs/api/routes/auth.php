9fdca9c55f1e7791dccb6a38a3b2170c5d05495e
Fady Mondy <EngFadyMondy@gmail.com>
2023-02-01 03:31:33 +0200

    first commit 🔥

--- stubs/api/routes/auth.php
+++ stubs/api/routes/auth.php
@@ -1,11 +1,11 @@
 <?php
 
-use breeze\stubs\default\app\Http\Controllers\Auth\AuthenticatedSessionController;
-use breeze\stubs\default\app\Http\Controllers\Auth\EmailVerificationNotificationController;
-use common\app\Http\Controllers\Auth\NewPasswordController;
-use common\app\Http\Controllers\Auth\PasswordResetLinkController;
-use common\app\Http\Controllers\Auth\RegisteredUserController;
-use common\app\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
--- stubs/splade/routes/auth.php
+++ stubs/splade/routes/auth.php
@@ -1,14 +1,14 @@
 <?php
 
-use breeze\stubs\default\app\Http\Controllers\Auth\AuthenticatedSessionController;
-use breeze\stubs\default\app\Http\Controllers\Auth\ConfirmablePasswordController;
-use breeze\stubs\default\app\Http\Controllers\Auth\EmailVerificationNotificationController;
-use breeze\stubs\default\app\Http\Controllers\Auth\EmailVerificationPromptController;
-use common\app\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordController;
-use common\app\Http\Controllers\Auth\PasswordResetLinkController;
-use common\app\Http\Controllers\Auth\RegisteredUserController;
-use common\app\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
